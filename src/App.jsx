f17d9ebd59737437daa7fb502bb04f2a8324f258
wiris316 <wiris316@gmail.com>
2023-08-03 18:15:35 -0400

    customized spin animation and pie chart

--- src/App.jsx
+++ src/App.jsx
@@ -9,25 +9,38 @@ import { motion } from "framer-motion"
 function App() {
   
   const randomNum = Math.floor(Math.random() * (359 - 1))
-  
+  const variants = {
+    rotate: { rotate: [0, 0, 720], transition: { repeat: 0, duration: 0.5 } },
+    stop: { y: [0, 0, 0], transition: { repeat: Infinity, repeatDelay: 0 } }
+  };
+  const [rotate, setRotate] = useState(false)
+
+  const handleSpin = () => {
+    setRotate(!rotate);
+  }
+
+  useEffect(() => {
+  }, [rotate])
 
   return (
     <>
       <ToDoBox />
+      <button onClick={handleSpin}>spin</button>
       <motion.div
-          className="box"
-          animate={{
+        className="box"
+        variants ={variants}
+        animate={
           // scale: [1, 2, 2, 1, 1],
           // rotate: [0, 0, 180, 0, 0],
-            rotate: [0, 0, 180 ]
+          rotate ? 'rotate' : 'stop'
           // borderRadius: ["0%", "0%", "50%", "50%", "0%"]
-          }}
-          transition={{
-            // duration: 1,
-            // ease: "easeInOut",
-            // times: [0, 0.2, 0.5, 0.8, 1],
-            // repeat: Infinity,
-            // repeatDelay: 0
+          }
+        transition={{
+          // duration: 1,
+          ease: "easeInOut",
+          times: [0, 0.2, 0.5, 0.8, 1],
+          // repeat: Infinity,
+          // repeatDelay: 0
           }}>
           <PieChart/>
       </motion.div>
--- src/components/PieChart.jsx
+++ src/components/PieChart.jsx
@@ -14,12 +14,31 @@ const PieChart = () => {
 
   return (
     <>
-      <Pie  height="500%"
-        options={{ maintainAspectRatio: false }}
+      <Pie
+        height="500%"
+        options={{
+          plugins: {
+            legend: {
+              display: false
+            }
+          },
+          maintainAspectRatio: false
+        }}
         data={{
-        // labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
         datasets: [{
-        data: data,
+          data: data,
+          backgroundColor:[
+            '#4dc9f6',
+            '#f67019',
+            '#f53794',
+            '#537bc4',
+            '#acc236',
+            '#166a8f',
+            '#00a950',
+            '#58595b',
+            '#8549ba'
+          ]
         // label: '# of Votes',
         // borderWidth: 1
         }]
