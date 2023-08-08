036df543828f03712cfbd99622e29aad2fd0e11f
wiris316 <wiris316@gmail.com>
2023-08-08 18:31:47 -0400

    edits to spin animation
    changed to spin one direction and stop at click of button

--- src/components/Animation.jsx
+++ src/components/Animation.jsx
@@ -8,7 +8,7 @@ import { motion } from "framer-motion"
 
 function Animation() {
   
-  const { rotate, setRotate, spinButton, setSpinButton } = useContext(StoreContext)
+  const { rotate, setRotate, spinButton, setSpinButton, result, setResult } = useContext(StoreContext)
   const [tempDegree,setTempDegree] = useState(0)
 
   const randomNum = Math.floor(Math.random() * (360)+1)
@@ -29,7 +29,7 @@ function Animation() {
   const variants = {
     rotate: {
       rotate: [0, 360],
-      scale: [1,0.98,1],
+      // scale: [1,0.98,1],
       transition: { repeat: Infinity, duration: 0.8, ease: 'linear' },
       // times: [5, 5, 0.5, 80, 80]// Adjust duration for smoother animation
     },
@@ -47,6 +47,7 @@ function Animation() {
   useEffect(() => {
     let rotationDegree = Math.floor(captureDegree())
     setTempDegree(rotationDegree)
+
   }, [rotate])
 
   return (
