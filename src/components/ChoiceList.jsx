7f25a858baff955a52f074b387c627c62bb1c532
wiris316 <wiris316@gmail.com>
2023-10-08 20:08:57 -0400

    code clean up

--- src/components/ChoiceList.jsx
+++ src/components/ChoiceList.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useContext, useEffect } from 'react';
+import { useState, useContext, useEffect } from 'react';
 import { StoreContext } from '../utils/store';
 import '../assets/ChoiceList.scss'
 import ConfirmResetDialog from './ConfirmResetDialog';
